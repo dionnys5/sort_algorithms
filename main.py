d6f5f9960582d1cd4d20ca6f0647df682d0a161d
Dionnys Santos Marinho <dionnys.marinho@gmail.com>
2019-05-03 22:24:06 -0300

    Teste algoritmos de ordenação

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-
 from Sorting import Sort
 
-lista = [4,5,1,2,8,2]
+with open('100000.csv', 'r') as lista:
+    lista = lista.read().split('\n')
+    print('lista unordered: ', lista)
 
-sorter = Sort(lista)
+lista_int = [int(x) for x in lista]
+sorter = Sort(lista_int)
 # sorter.BubbleOne()
-# sorter.BubbleTwo()
+sorter.BubbleTwo()
 # sorter.BubbleThree()
 # sorter.SelectionSort()
+# sorter.InsertionSort()
 
-
-
-print(lista)
+with open('relatorio_bubbleTwo100000.txt', 'w', encoding='UTF8') as file:
+    file.write('Total troca: ' + str(sorter.total_troca) + '\n')
+    file.write('Total comparações: ' + str(sorter.total_comparacoes) + '\n')
+    file.write('Total CPU: ' + str(sorter.cpu) + '\n')
+    file.write('Total memoria: ' + str(sorter.memory) + '\n')
+    file.write('Total Tempo: ' + str(sorter.time_exec))
+print('\n\n')
+print(sorter.lista)
